dabef81b8b67f7409cb4bbc0ab24e47cb4f15fd5
Georg Stam <georgstam@yahoo.com>
2012-09-07 16:14:42 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GergiApp::Application.config.secret_token = 'e8d5e67cdf8c1e5411f63e151cca3bacf2f03b708dddffad0d1795e42f09bf97038dfb19c60b4986df809792eec79166eb60ebad025eaf2491bbcfe640f55458'
